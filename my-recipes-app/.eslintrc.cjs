7554ba8a132317c250fa86c07edc626b52a4264f
NatUsik6 <93986148+NatUsik6@users.noreply.github.com>
2025-02-03 15:50:01 +0300

    MR-1-feat: create frontend app using React (#1)

---
+++ my-recipes-app/.eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: "eslint:recommended",
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
